1925a163ba0b1d8eafea67da99fff5fff1386444
Andrew Archer <andrew.m.archer@gmail.com>
2015-01-11 12:13:03 -0800

    property

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,9 @@
 class Event < ActiveRecord::Base
-	attr_accessor :formatted_time
 	belongs_to :vehicle
 
 	before_create :format_time
 
 	def format_time
-		self['formatted_time'] = self.created_at.to_datetime.strftime("%b %-d, %-I:%M%P")
+		self.formatted_time = self.created_at.to_datetime.strftime("%b %-d, %-I:%M%P")
 	end
 end
